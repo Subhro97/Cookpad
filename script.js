a318b6b78414c89475c94e1b399c4c5ba2e03895
Subhrojit Mondal <subhromond@gmail.com>
2021-06-04 14:28:31 +0530

    Foodelo

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
 const APP_ID = "50ee689a";
 const APP_KEY = "809453fdfa7a23d744a095740df6539b";
 
-const req = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+let req;
 let search = document.querySelector(".input");
+let recipes;
+let recipeNames;
+let recipeImg;
+let recipeUrl;
+let recipeIngre;
+
 
 const getData = async (e) => {
-  if (e.key == "Enter") {
-    console.log("Working");
+  if (e.key == "Enter" && search.value != "") {
+    req = `https://api.edamam.com/search?q=${search.value}&app_id=${APP_ID}&app_key=${APP_KEY}`;
     const res = await axios.get(req);
-    console.log(res);
+    recipes = res.data.hits;
+
+    if (recipes !== []) {
+      recipeNames = recipes.map((recipe) => {
+        return recipe.recipe.label;
+      });
+      recipeImg = recipes.map((recipe) => {
+        return recipe.recipe.image;
+      });
+      recipeUrl = recipes.map((recipe) => {
+        return recipe.recipe.url;
+      });
+      recipeIngre = recipes.map((recipe) => {
+        return recipe.recipe.ingredients;
+      });
+    }
+
+    let link=document.createElement("a");
+    link.href="recipes.html";
+    link.click();
   }
 };
 
-search.addEventListener("keypress",getData);
+search.addEventListener("keypress", getData);
